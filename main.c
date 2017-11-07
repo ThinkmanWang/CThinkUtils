20274783ae372e1d077f4c810d0d69b2d53f2508
Thinkman Wang <wangxf1985@gmail.com>
2017-11-08 01:50:55 +0800

    update

--- main.c
+++ main.c
@@ -1,11 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <glib.h>
+#include <unistd.h>
 
 #include "log.h"
 #include "test.h"
 #include "datetime.h"
+#include "ini.h"
 
+void test_ini()
+{
+    ini_t *config = ini_load("config.ini");
+    const char *name = ini_get(config, "owner", "name");
+    if (name) {
+        log_debug("name: %s\n", name);
+    }
+
+    const char *server = "default";
+    int port = 80;
+
+    ini_sget(config, "database", "server", NULL, &server);
+    ini_sget(config, "database", "port", "%d", &port);
+
+    log_debug("server: %s:%d\n", server, port);
+}
 
 int main()
 {
@@ -15,6 +33,12 @@ int main()
     char szDate[256];
     log_debug("%s\n", now(szDate));
     func1();
+
+    char buf[256];
+    getcwd(buf,sizeof(buf));
+    log_debug("current working directory: %s\n", buf);
+
+    test_ini();
 
     return 0;
 }
