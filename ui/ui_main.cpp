c1570736a8a64398740ac9d705ddd0591a6b5087
Thinkman Wang <wangxf1985@gmail.com>
2018-10-12 14:52:45 +0800

    update

--- ui/ui_main.cpp
+++ ui/ui_main.cpp
@@ -22,23 +22,12 @@ int test1(int argc, char** argv)
 
 int test_inputdialog(int argc, char** argv)
 {
-    /*
-     static int getInt(QWidget *parent
-                        , const QString &title
-                        , const QString &label
-                        , int value = 0
-                        , int minValue = -2147483647
-                        , int maxValue = 2147483647
-                        , int step = 1
-                        , bool *ok = nullptr
-                        , Qt::WindowFlags flags = Qt::WindowFlags());
-     */
     QApplication a( argc, argv );
     bool bOK = false;
     int nRet = QInputDialog::getInt(NULL
             , "Application name"
             , "Please enter a number"
-            , 1, -2147483647, 2147483647, 1
+            , 1, INT32_MIN, INT32_MAX, 1
             , &bOK);
 
     if (bOK) {
